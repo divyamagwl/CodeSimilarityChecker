c7b72c30992a0f88a15979ef0f6d2688d73823c8
Gagan Agarwal <Gagan.Agarwal@iiitb.ac.in>
2022-05-06 01:00:06 +0530

    Create class component for ast generator

--- generateAST.py
+++ generateAST.py
@@ -1,4 +1,49 @@
 import ast
+import sys
+from astor import to_source
 
+
+class RewriteVariableName(ast.NodeTransformer):
+    def visit_Name(self, node):
+        if(isinstance(node, ast.Name)):
+            result = ast.Name(id='var')
+            return ast.copy_location(result, node)
+        return node
 class generateAST:
-    pass
+
+    def readFile(self, filename):
+        with open(filename) as f:
+            contents = f.read()
+            return contents
+
+    def createAst(self, sourceCode):
+        inputASTtree = ast.parse(sourceCode)
+        newASTTree = RewriteVariableName().visit(inputASTtree)
+        newSourceCode = to_source(newASTTree)
+        # print(newSourceCode)
+        newASTTreeWithCtx = ast.parse(newSourceCode)
+        # print(ast.dump(newASTTree))
+        # print(ast.dump(newASTTreeWithCtx))
+        return newASTTreeWithCtx
+
+    def countExprType(self, tree, exprType):
+        count = 0
+        for node in ast.walk(tree):
+            if(isinstance(node, exprType)):
+                count += 1
+        return count
+
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+
+    generator = generateAST()
+    
+    sourceCode = generator.readFile(filename)
+    generatedASTtree = generator.createAst(sourceCode)
+    
+    loopsCount = generator.countExprType(generatedASTtree, (ast.For, ast.While))
+    ifCount = generator.countExprType(generatedASTtree, ast.If)
+    funcCount = generator.countExprType(generatedASTtree, ast.FunctionDef)
+
+    print(loopsCount, ifCount, funcCount)
