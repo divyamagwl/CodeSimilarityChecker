4ece98223a428135235b52fff23321966e6649e9
Divyam Agrawal <agrawaldivyam2001@gmail.com>
2022-05-06 00:25:51 +0530

    add: Count expression types

--- ast_generator.py
+++ ast_generator.py
@@ -18,14 +18,27 @@ def createAst(sourceCode):
     inputASTtree = ast.parse(sourceCode)
     newASTTree = RewriteVariableName().visit(inputASTtree)
     newSourceCode = to_source(newASTTree)
-    print(newSourceCode)
+    # print(newSourceCode)
     newASTTreeWithCtx = ast.parse(newSourceCode)
-    print(ast.dump(newASTTree))
-    print(ast.dump(newASTTreeWithCtx))
+    # print(ast.dump(newASTTree))
+    # print(ast.dump(newASTTreeWithCtx))
     return newASTTreeWithCtx
+
+def countExprType(tree, exprType):
+    count = 0
+    for node in ast.walk(tree):
+        if(isinstance(node, exprType)):
+            count += 1
+    return count
 
 
 if __name__ == '__main__':
     filename = sys.argv[1]
     sourceCode = readFile(filename)
-    input_tree = createAst(sourceCode)
+    generatedASTtree = createAst(sourceCode)
+    
+    loopsCount = countExprType(generatedASTtree, (ast.For, ast.While))
+    ifCount = countExprType(generatedASTtree, ast.If)
+    funcCount = countExprType(generatedASTtree, ast.FunctionDef)
+
+    print(loopsCount, ifCount, funcCount)
