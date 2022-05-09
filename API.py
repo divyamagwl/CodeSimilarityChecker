dd9d87df1ba0e28bd53e6929fcaf765b24dc5d22
vardhan2000 <vardhan.av2000@gmail.com>
2022-05-09 18:24:11 +0530

    added comments and made minor changes to API.py

--- API.py
+++ API.py
@@ -8,8 +8,10 @@ from io import BytesIO
 app = FastAPI()
 
 @app.post("/upload-file/")
-async def create_upload_file(file1: UploadFile = File(...),
+async def upload_file(file1: UploadFile = File(...),
                              file2: UploadFile = File(...)):
+    
+    # save both the input files locally
     file_location = f"./files/{file1.filename}"
     with open(file_location, "wb+") as file_object:
         file_object.write(file1.file.read())
@@ -18,12 +20,12 @@ async def create_upload_file(file1: UploadFile = File(...),
     with open(file_location, "wb+") as file_object:
         file_object.write(file2.file.read())
     
+    # run the code similarity checker for the saved input files
     ast_filePath = 'generateAST.py'
     os.system(f'python3 {ast_filePath} ./files/{file1.filename} ./files/{file2.filename} > temp.txt')
     
-    return FileResponse("temp.txt")
+    # return the txt file as response
+    return FileResponse("SimilarityScore.txt")
 
 if __name__ == "__main__":
     uvicorn.run(app, port=5500)
-
-# os.system('ls -l > temp.txt')
