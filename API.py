3abf5b4fc95c5b1ea8701f43496fa6e075f35808
vardhan2000 <vardhan.av2000@gmail.com>
2022-05-09 16:32:52 +0530

    return the output(similarity score) to the user in the api

--- API.py
+++ API.py
@@ -1,7 +1,7 @@
 import os
 import uvicorn
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import StreamingResponse
+from fastapi.responses import FileResponse
 from typing import Optional
 from io import BytesIO
 
@@ -18,11 +18,10 @@ async def create_upload_file(file1: UploadFile = File(...),
     with open(file_location, "wb+") as file_object:
         file_object.write(file2.file.read())
     
-    ast_filePath = '/home/aditya/Desktop/college_sems/6/PL/project/CodeSimilarityChecker/generateAST.py'
+    ast_filePath = 'generateAST.py'
     os.system(f'python3 {ast_filePath} ./files/{file1.filename} ./files/{file2.filename} > temp.txt')
     
-    # return {"info": f"file '{uploaded_file.filename}' saved at '{file_location}'"}
-
+    return FileResponse("temp.txt")
 
 if __name__ == "__main__":
     uvicorn.run(app, port=5500)
