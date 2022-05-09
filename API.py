19caad7dbd9d523be7061d6bcbf155fe5dc8bb58
Gagan Agarwal <Gagan.Agarwal@iiitb.ac.in>
2022-05-09 21:19:26 +0530

    Revert "Merge Conflict Resolved"
    This reverts commit 3470a7cda4783fabc5fa1562387c5b68af00397c, reversing
    changes made to 7238ad2223cf27dce812a0d6f5d967a976c69872.

--- API.py
+++ API.py
@@ -1,17 +1,15 @@
 import os
 import uvicorn
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import FileResponse
+from fastapi.responses import StreamingResponse
 from typing import Optional
 from io import BytesIO
 
 app = FastAPI()
 
 @app.post("/upload-file/")
-async def upload_file(file1: UploadFile = File(...),
+async def create_upload_file(file1: UploadFile = File(...),
                              file2: UploadFile = File(...)):
-    
-    # save both the input files locally
     file_location = f"./files/{file1.filename}"
     with open(file_location, "wb+") as file_object:
         file_object.write(file1.file.read())
@@ -20,12 +18,13 @@ async def upload_file(file1: UploadFile = File(...),
     with open(file_location, "wb+") as file_object:
         file_object.write(file2.file.read())
     
-    # run the code similarity checker for the saved input files
-    ast_filePath = 'generateAST.py'
+    ast_filePath = '/home/aditya/Desktop/college_sems/6/PL/project/CodeSimilarityChecker/generateAST.py'
     os.system(f'python3 {ast_filePath} ./files/{file1.filename} ./files/{file2.filename} > temp.txt')
     
-    # return the txt file as response
-    return FileResponse("SimilarityScore.txt")
+    # return {"info": f"file '{uploaded_file.filename}' saved at '{file_location}'"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, port=5500)
+
+# os.system('ls -l > temp.txt')
